843c3f9d3fc1226a773079d5b59edc7400986e7e
helloimzeus <44981267+helloimzeus@users.noreply.github.com>
2020-05-06 22:58:34 +0300

    personelller

--- personeller.Designer.cs
+++ personeller.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // personeller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "personeller";
+            this.ClientSize = new System.Drawing.Size(1039, 566);
+            this.Name = "personeller";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Personeller";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
