8da48b7966e410cb638e9981d8f3ef3eab896f13
ZIEatBabysZ <34131658+ZIEatBabysZ@users.noreply.github.com>
2020-05-23 00:17:36 +0300

    Bulunan Update(Form)

--- bulunan_urunler.Designer.cs
+++ bulunan_urunler.Designer.cs
@@ -294,6 +294,7 @@
             this.button5.TabIndex = 30;
             this.button5.Text = "Değişiklikleri Kaydet";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // bulunan_urunler
             // 
